d9594ddb55ce8ae674dd35c422594b006befc6dd
Emmanuel Azócar <azocarmel@gmail.com>
2022-03-14 10:49:01 -0400

    fix: added eslint rules

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,79 +1,79 @@
 export const getFromLSOrDefaultValue = (key, defaultValue) => {
-  const item = localStorage.getItem(key);
-  return item ? JSON.parse(item) : defaultValue;
+	const item = localStorage.getItem(key);
+	return item ? JSON.parse(item) : defaultValue;
 };
 
 export const createSearchFilter = (formData) => {
-  const { user, repository, typeOfSearch, condition, date } = formData;
+	const { user, repository, typeOfSearch, condition, date } = formData;
 
-  let searchCondition = "";
+	let searchCondition = '';
 
-  if (user && repository && typeOfSearch && condition && date) {
-    switch (condition) {
-      case "after":
-        searchCondition = ">=";
-        break;
-      case "before":
-        searchCondition = "<=";
-        break;
-    }
-  }
+	if (user && repository && typeOfSearch && condition && date) {
+		switch (condition) {
+		case 'after':
+			searchCondition = '>=';
+			break;
+		case 'before':
+			searchCondition = '<=';
+			break;
+		}
+	}
 
-  const searchTypeOfSearch = typeOfSearch ? `is:${typeOfSearch}` : undefined;
-  const searchDate =
+	const searchTypeOfSearch = typeOfSearch ? `is:${typeOfSearch}` : undefined;
+	const searchDate =
     searchCondition && date ? `closed:${searchCondition}${date}` : undefined;
 
-  return {
-    user,
-    repository,
-    typeOfSearch: searchTypeOfSearch,
-    date: searchDate,
-  };
+	return {
+		user,
+		repository,
+		typeOfSearch: searchTypeOfSearch,
+		date: searchDate,
+	};
 };
 
 const changeAnchorLink = (
-  anchorElement,
-  { user, repository, typeOfSearch, date }
+	anchorElement,
+	{ user, repository, typeOfSearch, date }
 ) => {
-  if (anchorElement) {
-    anchorElement.href = `https://github.com/${user}/${repository}/issues?q=${encodeURI(
-      `${typeOfSearch} ${date}`
-    )}`;
-    anchorElement.classList.remove("btn--disabled");
-  } else {
-    anchorElement.href = "#";
-    anchorElement.classList.add("btn--disabled");
-  }
+	if (anchorElement) {
+		anchorElement.href = `https://github.com/${user}/${repository}/issues?q=${encodeURI(
+			`${typeOfSearch} ${date}`
+		)}`;
+		anchorElement.classList.remove('btn--disabled');
+	} else {
+		anchorElement.href = '#';
+		anchorElement.classList.add('btn--disabled');
+	}
 };
 
 export const validateForm = (formData, anchorElement) => {
-  const filterData = createSearchFilter(formData);
+	const filterData = createSearchFilter(formData);
 
-  const isValid =
+	const isValid =
     filterData.user &&
     filterData.repository &&
     filterData.typeOfSearch &&
     filterData.date;
 
-  if (isValid && anchorElement) changeAnchorLink(anchorElement, filterData);
+	if (isValid && anchorElement) changeAnchorLink(anchorElement, filterData);
 
-  return isValid;
+	return isValid;
 };
 
 export const getFormValues = (form) => {
-  if (!form) return;
+	if (!form) return;
 
-  const values = {};
+	const values = {};
 
-  for (const input of Object.values(form)) {
-    try {
-      if (input.name.trim() && input.value.trim()) {
-        values[input.name] = input.value;
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
+	for (const input of Object.values(form)) {
+		try {
+			if (input.name.trim() && input.value.trim()) {
+				values[input.name] = input.value;
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}
 
-  return values;
+	return values;
 };
