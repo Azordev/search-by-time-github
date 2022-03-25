c3cdc38e36eb67371afe14d2774fdbffbce9ecd5
Emmanuel Azócar <azocarmel@gmail.com>
2022-03-25 21:05:15 +0000

    fixed some comments

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,49 +1,50 @@
 export const getFromLSOrDefaultValue = (key, defaultValue) => {
-	const item = localStorage.getItem(key);
-	return item ? JSON.parse(item) : defaultValue;
+  const item = localStorage.getItem(key);
+
+  return item ? JSON.parse(item) : defaultValue;
 };
 
 export const createSearchFilter = (formData) => {
-	const { user, repository, typeOfSearch, condition, date } = formData;
+  const { user, repository, typeOfSearch, condition, date } = formData;
 
-	let searchCondition = '';
+  let searchCondition = '';
 
-	if (user && repository && typeOfSearch && condition && date) {
-		switch (condition) {
-		case 'after':
-			searchCondition = '>=';
-			break;
-		case 'before':
-			searchCondition = '<=';
-			break;
-		}
-	}
+  if (user && repository && typeOfSearch && condition && date) {
+    switch (condition) {
+    case 'after':
+      searchCondition = '>=';
+      break;
+    case 'before':
+      searchCondition = '<=';
+      break;
+    }
+  }
 
-	const searchTypeOfSearch = typeOfSearch ? `is:${typeOfSearch}` : undefined;
-	const searchDate =
-		searchCondition && date ? `closed:${searchCondition}${date}` : undefined;
+  const searchTypeOfSearch = typeOfSearch ? `is:${typeOfSearch}` : undefined;
+  const searchDate =
+    searchCondition && date ? `closed:${searchCondition}${date}` : undefined;
 
-	return {
-		user,
-		repository,
-		typeOfSearch: searchTypeOfSearch,
-		date: searchDate,
-	};
+  return {
+    user,
+    repository,
+    typeOfSearch: searchTypeOfSearch,
+    date: searchDate,
+  };
 };
 
 const changeAnchorLink = (
-	anchorElement,
-	{ user, repository, typeOfSearch, date }
+  anchorElement,
+  { user, repository, typeOfSearch, date }
 ) => {
-	if (anchorElement) {
-		anchorElement.href = `https://github.com/${user}/${repository}/issues?q=${encodeURI(
-			`${typeOfSearch} ${date}`
-		)}`;
-		anchorElement.classList.remove('btn--disabled');
-	} else {
-		anchorElement.href = '#';
-		anchorElement.classList.add('btn--disabled');
-	}
+  if (anchorElement) {
+    anchorElement.href = `https://github.com/${user}/${repository}/issues?q=${encodeURI(
+      `${typeOfSearch} ${date}`
+    )}`;
+    anchorElement.classList.remove('btn--disabled');
+  } else {
+    anchorElement.href = '#';
+    anchorElement.classList.add('btn--disabled');
+  }
 };
 
 export const validateForm = () => {
@@ -65,34 +66,44 @@ export const validateForm = () => {
     },
   };
   const fields = document.querySelectorAll(
-    ".box.d-inline input,.box.d-inline select"
+    '.box.d-inline input,.box.d-inline select'
   );
 
-	const isValid =
-		filterData.user &&
-		filterData.repository &&
-		filterData.typeOfSearch &&
-		filterData.date;
+  let isValid = true;
+  fields.forEach((field) => {
+    const idField = field.getAttribute('id');
+    const spanError = document.getElementById('error_' + idField);
 
-	if (isValid && anchorElement) changeAnchorLink(anchorElement, filterData);
+    const value = field.value.trim();
+    if (fielValid[idField].required && !value) {
+      field.classList.add('error');
+      spanError.classList.add('span_error');
+      spanError.innerHTML = 'Please enter the required field';
+      isValid = false;
+    } else {
+      field.classList.remove('error');
+      spanError.classList.remove('span_error');
+      spanError.innerHTML = '';
+    }
+  });
 
-	return isValid;
+  return isValid;
 };
 
 export const getFormValues = (form) => {
-	if (!form) return;
+  if (!form) return;
 
-	const values = {};
+  const values = {};
 
-	for (const input of Object.values(form)) {
-		try {
-			if (input.name.trim() && input.value.trim()) {
-				values[input.name] = input.value;
-			}
-		} catch (error) {
-			console.error(error);
-		}
-	}
+  for (const input of Object.values(form)) {
+    try {
+      if (input.name.trim() && input.value.trim()) {
+        values[input.name] = input.value;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-	return values;
+  return values;
 };
